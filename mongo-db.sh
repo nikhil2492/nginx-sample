1ddd3f0d27116040492ca5c80aceea327d9482cf
nikhil <nikhil@creatiosoft.com>
2018-03-09 10:18:28 +0000

    updated

---
+++ mongo-db.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ansible-playbook /home/dev_ops/nginx-sample/mongo-db.yml >> ~/nginx-sample/fin.txt 
---
+++ mongo-log.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ansible-playbook /home/dev_ops/nginx-sample/mongo-log.yml >> /dev/null
